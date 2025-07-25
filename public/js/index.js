8d2de43df6264e7421168db07ad829eae8fb935e
dev-nirob1 <dev.nirob1@gmail.com>
2025-07-26 00:02:17 +0600

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 const navbarToggle = () => {
+  const navbar = document.querySelector(".navbar");
   const toggleMenu = document.querySelector(".hamburger");
-  const nav_links = document.querySelector("ul");
+  const nav_links = document.querySelector("nav-links");
   // navbar toggle
   toggleMenu.addEventListener("click", () => {
     if (nav_links.classList.contains("active")) {
@@ -9,5 +10,14 @@ const navbarToggle = () => {
       nav_links.classList.add("active");
     }
   });
+
+  // Add scroll animation to navbar
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 10) {
+      navbar.classList.add("scrolling");
+    } else {
+      navbar.classList.remove("scrolling");
+    }
+  });
 };
-navbarToggle()
+navbarToggle();
